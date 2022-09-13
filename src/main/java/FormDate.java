4d955b38538289be71aab9308e52947c06068a5e
Илья Задорин <zadorinilja@yandex.ru>
2022-09-13 21:29:49 +0400

    Birthday

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+public class FormDate {
+    public int birthday;
+
+
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phonne;
+    public String surname;
+    public  String subscription;
+
+    public FormDate birthday;
+}
